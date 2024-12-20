97e1175f21347bb5f406d547284f8c3f2fd37d4d
SADU VENKATESH <saduvenkatesh141205@gmail.com>
2024-12-20 09:05:03 +0530

    Header with Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
-
+import Header from "./components/Header.jsx"
 function App() {
   
 
   return (
     <>
-      
+      <Header/>
     </>
   )
 }
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import "./Header.css"
+
+export default function Header (){
+    return(
+        <header className="header">
+            <img src="../src/assets/farm_land.png" alt="farm-land" width="150px" className="farm-landimg" />
+            <nav>
+                <ul className="nav-list">
+                    <li className="nav-list-item"><a href="/Home">Home</a></li>
+                    <li className="nav-list-item"><a href="/Login">Login</a></li>
+                    <li className="nav-list-item"><a href="/Signup">Signup</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
