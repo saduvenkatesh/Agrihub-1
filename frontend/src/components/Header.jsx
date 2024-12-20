90456f17d146b5b5258abdf3b69bf96f8357a0e8
SADU VENKATESH <saduvenkatesh141205@gmail.com>
2024-12-20 09:22:23 +0530

    Making responsive Navbar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,6 +4,7 @@ export default function Header (){
     return(
         <header className="header">
             <img src="../src/assets/farm_land.png" alt="farm-land" width="150px" className="farm-landimg" />
+            <h1>Agrihub</h1>
             <nav>
                 <ul className="nav-list">
                     <li className="nav-list-item"><a href="/Home">Home</a></li>
